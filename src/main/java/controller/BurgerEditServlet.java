2da828df6077df211f0108c9a9671164bca11993
'DNyang1' <'rkddnwn3210@gmail.com'>
2025-11-03 14:36:41 +0900

    asd

--- src/main/java/controller/BurgerEditServlet.java
+++ src/main/java/controller/BurgerEditServlet.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class BurgerEditServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/burger/edit")
+@MultipartConfig
+public class BurgerEditServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
 }
