6d8885cc0a0845c59b0b4e7016d6c110a801b269
'DNyang1' <'rkddnwn3210@gmail.com'>
2025-11-04 16:20:25 +0900

    s

--- src/main/java/controller/BurgerDetailsServlet.java
+++ src/main/java/controller/BurgerDetailsServlet.java
@@ -1,9 +1,12 @@
 package controller;
 
 import java.io.IOException;
-import jakarta.servlet.*;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import dao.BurgerDAO;
 import dto.BurgerDTO;
 
